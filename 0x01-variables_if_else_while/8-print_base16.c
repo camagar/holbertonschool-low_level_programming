aad76c6b8815b2e76bd0c610b74afebe84e30cae
camagar <mauriciogrestrepo@gmail.com>
2020-06-12 04:48:29 +0000

    -print base 16-

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,12 @@
 int main(void)
 {
 	int a;
+	char b;
 
-	for (a = 0; a <= 16; a++)
+	for (a = 0; a <= 9; a++)
 		putchar('0' + a);
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
 	putchar('\n');
 	return (0);
 }
