1efe84ed62e7f8033f4542ad58b80d0a316ba9c9
camagar <mauriciogrestrepo@gmail.com>
2020-06-12 01:33:23 +0000

    print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char aph;
+
+	for (aph = 'a'; aph <= 'z'; aph++)
+		putchar(aph);
+	putchar('\n');
+	return (0);
+
+}
