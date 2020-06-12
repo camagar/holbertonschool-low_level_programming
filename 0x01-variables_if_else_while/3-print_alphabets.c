14dd7920add477e1f5c909233fba9e9c4b13a32f
camagar <mauriciogrestrepo@gmail.com>
2020-06-12 02:51:18 +0000

    print 2 alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alf, BT;
+
+	for(alf = 'a'; alf <= 'z'; alf++)
+		putchar(alf);
+	for(BT = 'A'; BT <= 'Z'; BT++)
+		putchar(BT);
+	putchar('\n');
+	return(0);
+}
