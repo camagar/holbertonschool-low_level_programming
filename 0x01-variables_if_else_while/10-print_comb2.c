eba145b4e5e6bdec08301a0d9ba6e9f67a03718e
camagar <mauriciogrestrepo@gmail.com>
2020-06-12 20:12:55 +0000

    print number 0-100

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			if (a != 9 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
