2fd8aebba40c306d098670b870ec1acd154dd754
camagar <mauriciogrestrepo@gmail.com>
2020-06-16 23:08:19 +0000

    print the value of last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *@c: variable to check
+ * Return: Always 0.
+ */
+int print_last_digit(int c)
+{
+	c = c % 10;
+	if (c < 0)
+		c = c * -1;
+	return (c);
+}
