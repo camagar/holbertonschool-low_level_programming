b5e27ecdb241d97de6b5dfc19e631d46c3f0d4c6
camagar <mauriciogrestrepo@gmail.com>
2020-06-17 00:43:06 +0000

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *@a: first number to add
+ *@b: second number to add
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+
+}
