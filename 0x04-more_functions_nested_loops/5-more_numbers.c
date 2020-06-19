0f8c5f0ece8035ad4e504179a78770e8d1d10f33
camagar <mauriciogrestrepo@gmail.com>
2020-06-19 02:23:28 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	while (b < 10)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			if ((a / 10) > 0)
+		{
+			_putchar((a / 10) + '0');
+		}
+		_putchar((a % 10) + '0');
+		}
+		b++;
+		_putchar('\n');
+	}
+	
+}
