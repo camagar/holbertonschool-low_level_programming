73c3fd95a64fbbe0455efa78dfb33b6ae3f9e86c
camagar <mauriciogrestrepo@gmail.com>
2020-06-19 04:36:39 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ * @size: variable to check
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+}
