c7cdf6568fe2aa8b5e91821e8c8ffd12cd9d301e
camagar <mauriciogrestrepo@gmail.com>
2020-06-20 04:14:52 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * print_number - check the code for Holberton School students.
+ * @n: number to print
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	char l;
+	int r;
+	char d;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		l = ('0' - (n % 10));
+		n /= -10;
+	} else
+	{
+		l = ((n % 10) + '0');
+		n /= 10;
+	}
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n /= 10;
+	}
+
+	while (r > 0)
+	{
+		d = ((r % 10) + '0');
+		_putchar (d);
+		r /= 10;
+	}
+	_putchar(l);
+
+}
