30861a4db4b75fe77c1b601f52f2e71fb3f9a3f8
camagar <mauriciogrestrepo@gmail.com>
2020-06-23 19:37:22 +0000

    count the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: variable to calculate length
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	return (i);
+}
