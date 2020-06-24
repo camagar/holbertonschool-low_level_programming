fa1b613a1a178f5294031f73a2552b6c83d445b5
camagar <mauriciogrestrepo@gmail.com>
2020-06-24 01:51:27 +0000

    Print Reverse string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rev_string - print reverse string.
+ * @s: variable to print reverse string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char k;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	}
+	j = 0;
+	while (i >= j)
+	{
+		i--;
+		k = *(s + j);
+		*(s + j) = *(s + i);
+		*(s + i) = k;
+		j++;
+	}
+
+}
