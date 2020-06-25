e75081bd20f66e40b2c21858e4e3535f06938605
camagar <mauriciogrestrepo@gmail.com>
2020-06-25 22:31:10 +0000

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,13 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	i = 0;
-	while (*(dest + i) != '\0')
+	for (i = 0; *(dest + i) != '\0'; i++)
 	{
-		i++;
 	}
-	j = 0;
-	while (*(src + j) != '\0')
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		*(dest + i) = *(src + j);
-		j++;
+		i++;
 	}
 	return (dest);
 }
