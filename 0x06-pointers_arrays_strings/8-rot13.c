248da1e9a4f66cb00db092f570927fbc6e3670d1
camagar <mauriciogrestrepo@gmail.com>
2020-06-27 04:02:27 +0000

    encoding to rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rot13 - enconde the text to rot13.
+ * @p: pointer.
+ * Return: text encoding.
+ */
+char *rot13(char *p)
+{
+	int i, j;
+	char rot1[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char rot2[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		for (j = 0; j < 53; j++)
+		{
+			if (p[i] == rot1[j])
+			{
+				p[i] = rot2[j];
+			}
+		}
+	}
+	return (p);
+}
