862b51b67fbbe3fafead2b1cbdbcb48e6a892aad
camagar <mauriciogrestrepo@gmail.com>
2020-07-02 23:47:13 +0000

    Print reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: pointer to string.
+ * Return: string.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
