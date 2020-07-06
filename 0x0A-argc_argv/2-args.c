25c336046a4c6e3dee6625619c1cd900babde88a
camagar <mauriciogrestrepo@gmail.com>
2020-07-06 03:01:53 +0000

    number 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments.
+ *  @argc: int
+ *  @argv: Char
+ *  Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
