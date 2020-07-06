af6875ab4c90c3eba15494e00d2de4b4b15bbc1b
camagar <mauriciogrestrepo@gmail.com>
2020-07-06 02:40:52 +0000

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print its name.
+ *  @argc: int
+ *  @argv: Char
+ *  Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+	return (argc);
+}
