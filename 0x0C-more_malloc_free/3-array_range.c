5580332928a13615b4c3ebb1d53c5c414513ac2d
camagar <mauriciogrestrepo@gmail.com>
2020-07-10 03:59:14 +0000

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: minnimum
+ * @max: maximum
+ *
+ * Return: the pointer to the newly created array.
+ */
+int *array_range(int min, int max)
+{
+	int *a;
+	int i, j;
+
+	j = 0;
+	if (min > max)
+	{
+		return (NULL);
+	}
+	a = (int *)malloc((max - min) * sizeof(int));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = min; i <= max; i++)
+	{
+		a[j] = i;
+	}
+	return (a);
+}
