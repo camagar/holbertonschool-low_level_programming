6d12b106fdc85cba6d256e0759b4fb3390caba8f
camagar <mauriciogrestrepo@gmail.com>
2020-07-15 04:24:45 +0000

    preprocessor abuse

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,9 @@
+#ifndef preprocessor_abuse
+#define preprocessor_abuse "Hello, Holberton"
+#endif /* preprocesor_abuse */
+#include <stdio.h>
+
+void main(int argc, char *argv[])
+{
+	printf("%s\n", preprocessor_abuse);
+}
