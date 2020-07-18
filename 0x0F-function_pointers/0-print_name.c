21ad4396eae8939de25f59877b37302543442c93
camagar <mauriciogrestrepo@gmail.com>
2020-07-18 02:37:19 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: name of the person
+ * @f: pointer to a function.
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+	(f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef Function_pointers
+#define Function_pointers
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* Function_pointers */
