e49e0a3d317df0659ec98e4e81d086dd8269708a
camagar <mauriciogrestrepo@gmail.com>
2020-07-18 03:50:11 +0000

    index.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,25 +11,22 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, j;
+	int i, count;
 
+	count = 0;
 	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 			{
-				j++;
+				count++;
 				return (i);
 			}
 		}
 	}
-	if (size <= 0 || j == 0)
-	{
+	if (size <= 0 || count == 0)
 		return (-1);
-	}
 	else
-	{
 		return (0);
-	}
 }
