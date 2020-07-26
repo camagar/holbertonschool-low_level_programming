4156e82285fb238a3d88f9a6f1db0bd6eb228838
camagar <mauriciogrestrepo@gmail.com>
2020-07-26 20:07:05 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - print the elements of the list
+ * @h: is a pointer
+ * 
+ * Return: the elements
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			h = h->next;
+			i++;
+		}
+		else
+		{
+		printf("[%d] %s", h->len, h->str);
+		i++;
+		h = h->next;
+		printf("\n");
+		}
+	}	
+	return (i);
+}
