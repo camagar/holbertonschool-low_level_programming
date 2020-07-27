c25f427b01065ab9bc2664550103db34804a32d7
camagar <mauriciogrestrepo@gmail.com>
2020-07-27 01:21:25 +0000

    print first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * Firstof_all - print first the main funct.
+ *
+ * Return: no return
+ */
+void __attribute__((constructor)) Firstof_all()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
