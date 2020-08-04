22eae7b4106faf3aa85fb2120050b10d9177050a
camagar <mauriciogrestrepo@gmail.com>
2020-08-04 02:35:29 +0000

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,29 +8,25 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+
 	struct listint_s *end = NULL;
 	struct listint_s *aux = NULL;
 
 	end = (struct listint_s *)malloc(sizeof(struct listint_s));
 	if (end == NULL)
-	{
 		return (NULL);
-	}
 	end->n = n;
 	end->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = end;
-	}
-	else
+	} else
 	{
 		aux = *head;
 		while (aux->next != NULL)
-		{
 			aux = aux->next;
-		}
 		aux->next = end;
 	}
 	return (end);
+
 }
