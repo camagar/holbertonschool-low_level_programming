e24986228ef04da622dc8ff69ed81e5166658857
camagar <mauriciogrestrepo@gmail.com>
2021-02-23 02:02:14 +0000

    island - perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
-"""module to find island perimeter"""
+""" create function """
 
 
 def island_perimeter(grid):
-    """returns the perimeter of island described in grid"""
-    p = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                if row - 1 < 0:
-                    p += 1
-                else:
-                    try:
-                        if grid[row - 1][col] == 0 or row - 1 < 0:
-                            p += 1
-                    except:
-                        pass
-                if row + 1 > len(grid) - 1:
-                    p += 1
-                else:
-                    try:
-                        if grid[row + 1][col] == 0:
-                            p += 1
-                    except:
-                        pass
-                if col + 1 > len(grid[row]) - 1:
-                    p += 1
-                else:
-                    try:
-                        if grid[row][col + 1] == 0:
-                            p += 1
-                    except:
-                        pass
-                try:
-                    if grid[row][col - 1] == 0 or col - 1 < 0:
-                        p += 1
-                except:
-                    pass
-    return (p)
+    """ returns the perimeter of the island described in grid """
+
+    count = 0
+    count2 = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                count += 1
+                if i != 0 and grid[i - 1][j] == 1:
+                    count2 += 1
+                if j != 0 and grid[i][j - 1] == 1:
+                    count2 += 1
+
+    return 4 * count - 2 * count2
