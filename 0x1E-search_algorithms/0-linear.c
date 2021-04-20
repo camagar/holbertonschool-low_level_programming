2616a38d8187d058d20fc13816e92645b8465145
camagar <mauricio@MAURICIO.localdomain>
2021-04-20 13:30:28 -0500

    fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_search - Entry point
- * Desc: Entry
- * @array: is a pointer to the first element of the array to search
- * @size:  is the number of elements in array
- * @value: is the value to search for
- * Return: a pointer to the newly allocated hashtable
+ * linear_search - searches for a value in a array of integers.
+ * @array: pointer to the array
+ * @size: size of the array
+ * @value: value to search
+ * Return: the pointer to a new node at the beginning of a dlistint_t list
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
 	if (!array)
-	{
 		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, (int)array[i]);
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 		if (value == array[i])
-		{
-			return ((int)i);
-		}
+			return (i);
 	}
 	return (-1);
 }
